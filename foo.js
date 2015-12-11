0206098dfa25399d7f148938ad5031e3728f8d92
John Crescenzi <john.crescenzi@temple.edu>
2015-12-10 21:19:16 -0500

    Homepage
    What I have done so far of homepage

---
+++ foo.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+	var vid = document.getElementById("myVideo");
+		vid.loop = true;
+  	var index = 1,
+      playlist = ['http://ia700804.us.archive.org/20/items/StarsTimeLapse/Stars.mp4'],
+      video = document.getElementById('bgvid');
+</script>
